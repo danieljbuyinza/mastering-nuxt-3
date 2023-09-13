e13c7f628fea0e61462cbdab94ffc44bc4c2af8b
Daniel Buyinza <danieljbuyinza@gmail.com>
2023-09-13 07:38:19 +0300

    Remove unneeded dependencies, add postcss snippet to fix netlify build.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,14 @@ export default defineNuxtConfig({
 		},
 	},
 	modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+			cssnano:
+				process.env.NODE_ENV === "production"
+					? { preset: ["default", { discardComments: { removeAll: true } }] }
+					: false, // disable cssnano when not in production
+		},
+	},
 });
